e6c90793f131bd209e8c9afb63020581d606b9cc
Evgeniy Tomashevich <evg.tomash@gmail.com>
2024-07-07 22:36:06 +0300

    Added an OrderFragment

---
+++ app/src/main/java/com/tomashevich/bitsandpizza/OrderFragment.kt
@@ -0,0 +1,20 @@
+package com.tomashevich.bitsandpizza
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+
+class OrderFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_order, container, false)
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        return view
+    }
+}
